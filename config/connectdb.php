b9aad2db50b0091298b32c1ab1244862922f08eb
66010914142 <66010914142@msu.ac.th>
2026-02-16 17:32:27 +0700

    TES

--- config/connectdb.php
+++ config/connectdb.php
@@ -2,7 +2,7 @@
 		$host = "localhost";
 		$user = "root";
 		$pwd = "12345678";
-		$db = "meatshop";
+		$db = "MEATSHOP";
 		$conn = mysqli_connect($host,$user,$pwd,$db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 		mysqli_query($conn, "SET NAMES utf8");
 		?>
