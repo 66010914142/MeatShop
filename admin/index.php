c436a0af034c6dbb4d70f54730e2dd32fb5e8e7f
66010914142 <66010914142@msu.ac.th>
2026-03-10 10:20:44 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -6,7 +6,7 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login: พาฤดี ปูนจีน (จ๋า)</title>
+    <title>Login Admin</title>
     <link href="https://fonts.googleapis.com/css2?family=Sarabun:wght@300;400;600&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
